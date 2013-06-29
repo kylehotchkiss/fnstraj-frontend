8b9ae002ac3c6d3ad1696bb9e75bd218a0a50308
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2013-06-29 16:46:41 -0400

    Version Bump

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,19 @@
 		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 		<script type="text/javascript" src="//use.typekit.net/ses4lbp.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-39937936-3']);
+			_gaq.push(['_setSiteSpeedSampleRate', 100]);			
+			_gaq.push(['_trackPageview']);
+	
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+	  	</script>
 	</head>
 	<body>
 		<div class="header">
@@ -40,3 +53,4 @@
 				</ul>
 			</div>
 		</div>
+<?php ob_flush(); flush(); ?>
